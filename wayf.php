fae2eed03006dee4aabf0f5ae56b1f9ce45a5da8
monnardj <jacques.monnard@unifr.ch>
2018-03-20 14:35:17 +0100

    Switch for moodle2/moodle-test
    Added switch to detect server and create appropriate login links
    automatically for moodle2 or moodle-test

--- wayf.php
+++ wayf.php
@@ -2,12 +2,17 @@
 defined('MOODLE_INTERNAL') || die();
 
 ob_start();
+if (strpos($CFG->wwwroot,'moodle2') === false) {
+    $login = "https://moodle-test.unifr.ch/Shibboleth.sso/Login?entityID=https%3A%2F%2Ftestidp.unifr.ch%2Fidp%2Fshibboleth&target=https%3A%2F%2Fmoodle-test.unifr.ch%2Fauth%2Fshibboleth%2Findex.php";
+} else {
+    $login = "https://moodle2.unifr.ch/Shibboleth.sso/Login?entityID=https%3A%2F%2Faai.unifr.ch%2Fidp%2Fshibboleth&target=https%3A%2F%2Fmoodle2.unifr.ch%2Fauth%2Fshibboleth%2Findex.php";
+}
 ?>
     <div class="loginform">
-        <div class="form-input unifr-form"><a rel="external" href="https://moodle2.unifr.ch/Shibboleth.sso/Login?entityID=https%3A%2F%2Faai.unifr.ch%2Fidp%2Fshibboleth&target=https%3A%2F%2Fmoodle2.unifr.ch%2Fauth%2Fshibboleth%2Findex.php"><img src="<?php echo $CFG->wwwroot;?>/theme/clean_unifr/pix/unifr_login.gif" class="unifr_button"></a></div>
+        <div class="form-input unifr-form"><a rel="external" href="<?php echo $login ?>"><img src="<?php echo $CFG->wwwroot;?>/theme/clean_unifr/pix/unifr_login.gif" class="unifr_button"></a></div>
 
 <!--			<div class="form-label unifr-label2"><?php print_string('otherAAI','local_unifr_lang') ?></div>-->
-        <div class="form-input unifr-button unifr-form"><a rel="external" href="https://moodle2.unifr.ch/auth/shibboleth/index.php"><img src="<?php echo $CFG->wwwroot;?>/theme/clean_unifr/pix/switchaai_login.gif" class="unifr_button"></a></div>
+        <div class="form-input unifr-button unifr-form"><a rel="external" href="<?php echo $CFG->wwwroot;?>/auth/shibboleth/index.php"><img src="<?php echo $CFG->wwwroot;?>/theme/clean_unifr/pix/switchaai_login.gif" class="unifr_button"></a></div>
 <!--			<div class="form-label unifr-label3"><?php print_string('external','local_unifr_lang') ?></div>-->
         <div class="form-input unifr-form"><button class="unifr-normalbutton" type="button" value="Compte standard" onclick="document.location.href='<?php echo $CFG->wwwroot?>/login/index.php?ext=1'">&nbsp;&nbsp;&nbsp;&nbsp;<?php print_string('externalaccount','local_unifr_lang') ?>&nbsp;&nbsp;&nbsp;&nbsp;</button></div>
 <!--			<div class="form-label unifr-label3"><?php print_string('guest','local_unifr_lang') ?></div>-->
